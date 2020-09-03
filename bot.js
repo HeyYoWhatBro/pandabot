11b19112cad1d35bc3f5fa9c02da0729a199c58b
HeyYoWhatBro <69439251+HeyYoWhatBro@users.noreply.github.com>
2020-09-04 02:11:58 +0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,51 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const preifix = '&'
+const fs = require('fs');
+
+client.commands = new Discord.Collection()
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+  const command = require(`./commands/${file}`)
+
+  client.commands.set(command.name, command);
+
+}
+client.once('ready', () => {
+  console.log('ready to online');
+});
+client.on('message', message =>{
+  if(!message.content.startsWith(preifix) || message.author.bot) return;
+
+  const args = message.content.slice(preifix.length).split(/ =/);
+  const command = args.shift().toLowerCase();
+
+  if(command === 'help')
+  client.commands.get('help').execute(message, args)
+  
+  if(command === 'grup')
+  client.commands.get('grup').execute(message, args)
+  
+  if(command === 'vote')
+  client.commands.get('vote').execute(message, args)
+
+  if(command === 'liststaff')
+  client.commands.get('liststaff').execute(message, args)
+
+  if(command === 'ip')
+  client.commands.get('ip').execute(message, args)
+});
+
+client.on("guildMemberAdd", member =>{
+
+    const welcome = new Discord.MessageEmbed()
+
+    .setTitle('selamat datang di otsuCraft Discord')
+    .addFields(
+      { name: 'Halo Nama saya pandabot', value: 'saya adalah bot dari pandabot', inline: false},
+      { name: 'Untuk tau Command dari bot ini', value: 'Tulis &help', inline: false},
+    )
+    member.send(welcome);
+});
+client.login(process.env.token);
